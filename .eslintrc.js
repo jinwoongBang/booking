50f18fdd81c5376edc9cc2dc0ca15412fbac4537
jinwoongBang <dlopo123@gmail.com>
2020-11-15 17:57:49 +0900

    eslint, prettier 환경설정 추가

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'prettier',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier/react',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+};
